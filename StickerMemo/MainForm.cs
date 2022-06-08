31f67983130f2c7ac73f251998d66042bcb59a41
deveb1479@gmail.com <deveb1479@gmail.com>
2022-06-08 10:39:52 +0900

    auto save fix

--- StickerMemo/MainForm.cs
+++ StickerMemo/MainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,7 +20,7 @@ namespace StickerMemo
             this.Resize += (_sender, _e) =>
             {
                 this.main_text.Size
-                = this.ClientSize - new Size(this.main_text.Margin.Horizontal * 4, this.main_text.Margin.Vertical * 5 - this.menu.Size.Height);
+                = this.ClientSize - new Size(this.main_text.Margin.Horizontal * 4, this.main_text.Margin.Vertical * 4 - this.menu.Size.Height);
             };
 
             this.FormClosed += (_sender, _e) =>
@@ -28,9 +29,14 @@ namespace StickerMemo
                 Properties.Settings.Default.Save();
             };
 
+            this.StartPosition = FormStartPosition.CenterScreen;
+
             this.menu.Items[0].Click += (_sender, _e) =>
             {
-                this.main_text.Text = "";
+                if (MessageBox.Show("You want to reset?", "caption", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    this.main_text.Text = "";
+                }
             };
 
             this.menu.Items[1].Click += (_sender, _e) =>
@@ -38,8 +44,20 @@ namespace StickerMemo
                 this.TopMost = !this.TopMost;
             };
 
-            this.main_text.Font = new Font("굴림체", 11, FontStyle.Regular);
+            this.main_text.Font = new Font("굴림", 11, FontStyle.Regular);
             this.main_text.SelectionFont = this.main_text.Font;
+
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+
+            t.Interval = 5000;
+
+            t.Tick += (sender, e) =>
+            {
+                Properties.Settings.Default.MainMemo = this.main_text.Text;
+                Properties.Settings.Default.Save();
+            };
+
+            t.Start();
         }
 
         private void LoadForm(object sender, EventArgs e)
